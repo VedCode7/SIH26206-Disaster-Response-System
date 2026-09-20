e0f5f34bffb5fec7a39a0ee591d462e86a544e6e
VedCode7 <vedant.singh9b@gmail.com>
2026-09-20 10:54:45 +0530

    feat: expose resource facility model

--- backend/app/domain/models/resources/__init__.py
+++ backend/app/domain/models/resources/__init__.py
@@ -1,9 +1,11 @@
 from backend.app.domain.models.resources.allocation import ResourceAllocation
 from backend.app.domain.models.resources.demand import ResourceDemand
 from backend.app.domain.models.resources.resource import Resource
+from backend.app.domain.models.resources.resource_facility import ResourceFacility
 
 __all__ = [
     "Resource",
+    "ResourceFacility",
     "ResourceDemand",
     "ResourceAllocation",
 ]
